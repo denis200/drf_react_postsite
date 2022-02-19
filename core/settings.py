e6589c0d71b66fb5704a456309de12e11921ad0c
Denis Komarkovu <soundze@mail.ru>
2022-02-19 12:49:01 +0300

    Added jwauthorization

--- core/settings.py
+++ core/settings.py
@@ -40,7 +40,8 @@ INSTALLED_APPS = [
     'blog',
     'blog_api',
     'rest_framework',
-    'corsheaders'
+    'corsheaders',
+    'users'
 ]
 
 MIDDLEWARE = [
@@ -133,8 +134,11 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK ={
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticatedOrReadOnly',
-]
+        'rest_framework.permissions.AllowAny',
+],
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
 }
 
 CORS_ALLOWED_ORIGINS = [
@@ -143,3 +147,5 @@ CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000',
     'https://127.0.0.1:3000'
 ]
+
+AUTH_USER_MODEL = "users.NewUser"
